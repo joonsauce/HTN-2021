c2ae51734f0f6580ac2599e16148de1edd89775e
Joon <77248354+joonsauce@users.noreply.github.com>
2021-09-18 23:42:16 -0400

    removed unnecessary code

--- HTN 2021 Backend/cockroach/dependencies/models.py
+++ HTN 2021 Backend/cockroach/dependencies/models.py
@@ -2,29 +2,10 @@ from django.db import models
 import uuid
 
 
-class Customers(models.Model):
+class Users(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False)
     name = models.CharField(max_length=250)
-
-
-class Products(models.Model):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False)
-    name = models.CharField(max_length=250)
-    price = models.DecimalField(max_digits=18, decimal_places=2)
-
-
-class Orders(models.Model):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False)
-    subtotal = models.DecimalField(max_digits=18, decimal_places=2)
-    customer = models.ForeignKey(
-        Customers, on_delete=models.CASCADE, null=True)
-    product = models.ManyToManyField(Products)
+    num = models.CharField(max_length=2)
