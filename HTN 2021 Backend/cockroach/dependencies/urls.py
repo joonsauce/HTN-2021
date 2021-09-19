423f296005d02070f65da591e8da371e31a3cbef
Joon <77248354+joonsauce@users.noreply.github.com>
2021-09-18 23:41:55 -0400

    removed unnecessary code

--- HTN 2021 Backend/cockroach/dependencies/urls.py
+++ HTN 2021 Backend/cockroach/dependencies/urls.py
@@ -1,20 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import CustomersView, OrdersView, PingView, ProductView
+from .views import UsersView, PingView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('ping/', PingView.as_view()),
 
-    # Endpoints for customers URL.
-    path('customer/', CustomersView.as_view(), name='customers'),
-    path('customer/<uuid:id>/', CustomersView.as_view(), name='customers'),
-
-    # Endpoints for customers URL.
-    path('product/', ProductView.as_view(), name='product'),
-    path('product/<uuid:id>/', ProductView.as_view(), name='product'),
-
-    path('order/', OrdersView.as_view(), name='order'),
+    # Endpoints for user URL.
+    path('users/', UsersView.as_view(), name='users'),
+    path('users/<uuid:id>/', UsersView.as_view(), name='users')
 ]
