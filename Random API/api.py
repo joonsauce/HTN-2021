a25583b463f9d121e65738367e40fef2ee494e66
Joon <77248354+joonsauce@users.noreply.github.com>
2021-09-19 03:36:15 -0400

    more connections

--- Random API/api.py
+++ Random API/api.py
@@ -1,6 +1,7 @@
 import ast
 import pandas as pd
 import random
+import requests
 from data import *
 from flask import Flask
 from flask_restful import Api, reqparse, Resource
@@ -16,11 +17,34 @@ class nums(Resource):
 
 class names(Resource):
     def get(self):
-        data = ticket[randint(0,3)]
+        data = ticket[randint(0,len(ticket) - 1)]
         return {'data': data}, 200
+
+class getDelInfo(Resource):
+    def get(self):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        response = requests.get('http://<server-ip>/users', headers=headers)
+        for i in range(len(response.json())):
+            requests.delete(str('http://<server-ip>/users/' + response.json()[i]['id']), headers=headers)
+        return response.json(), 200
+
+class getPersonInfo(Resource):
+    def get(self):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        response = requests.get('http://<server-ip>/users', headers=headers)
+        number = randint(0, len(response.json()) - 1)
+        name = response.json()[number]['name']
+        num = response.json()[number]['num']
+        return {'name': name, 'num': num}, 200
 
 api.add_resource(nums, '/nums')
 api.add_resource(names, '/names')
+api.add_resource(getDelInfo, '/getDelInfo')
+api.add_resource(getPersonInfo, '/getPersonInfo')
 
 if __name__ == '__main__':
-    app.run(host="<your-ip-here>")
+    app.run(host="<local-ip>")
