b7b29726b0c3be7db135483c00212b53e06dcc87
Joon <77248354+joonsauce@users.noreply.github.com>
2021-09-19 03:35:31 -0400

    more names!

--- Random API/data.py
+++ Random API/data.py
@@ -1,1 +1,6 @@
-ticket = ["Brandon", "Farzad", "Jerome", "Joon"]
+ticket = ["Brandon", "Farzad", "Jerome", "Joon", "Nicholas", "Justin", "Erin", "Jagmeet", "Yves-François", "Annamie",
+          "Lionel", "Kylian", "Alex", "Alexey", "Felix", "Sowad", "Omar", "Mesut", "Donald", "Hilary", "Barack",
+          "Joe", "Jonathan", "Amir", "Anthony", "Ayazhan", "Carol", "Danielle", "Jimmy", "Michael", "Nabil",
+          "Rohan", "Tariq", "Ajay", "Andy", "Arjang", "Nic", "Corbin", "Deon", "Evan", "Henry", "Jacob", "Jake", "Jen",
+          "Jordan", "Kaitlyn", "Kartik", "Katherine", "Kevin", "Mustafa", "Nima", "Qasar", "Ruchir", "Sean",
+          "Sunil", "Victor", "Vishal", "Carlos", "Gautam", "Harshyt", "Kate"," Mahyar", "Matthieu", "Syam", "Tim"]
