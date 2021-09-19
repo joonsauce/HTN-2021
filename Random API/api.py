f47eb2dbf4035910f75d6fb6d0766f966c92f9fc
Joon <77248354+joonsauce@users.noreply.github.com>
2021-09-18 23:44:36 -0400

    rng

---
+++ Random API/api.py
@@ -0,0 +1,26 @@
+import ast
+import pandas as pd
+import random
+from data import *
+from flask import Flask
+from flask_restful import Api, reqparse, Resource
+from random import randint
+
+app = Flask(__name__)
+api = Api(app)
+
+class nums(Resource):
+    def get(self):
+        data = randint(1, 10)
+        return {'data': data}, 200
+
+class names(Resource):
+    def get(self):
+        data = ticket[randint(0,3)]
+        return {'data': data}, 200
+
+api.add_resource(nums, '/nums')
+api.add_resource(names, '/names')
+
+if __name__ == '__main__':
+    app.run(host="192.168.0.192")
---
+++ Random API/data.py
@@ -0,0 +1,1 @@
+ticket = ["Brandon", "Farzad", "Jerome", "Joon"]
